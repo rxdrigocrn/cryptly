f6fd839adbff58730b755c7ded9dbd63d75b0ba3
rxdrigocrn <rodrigocirinocl@gmai.com>
2024-08-23 09:37:39 -0300

    coin_page

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import "./Coin.css"
 function Coin({ coins }) {
   const { coinId } = useParams();
   const coin = coins.find(c => c.id === coinId);
@@ -11,12 +11,20 @@ function Coin({ coins }) {
 
   return (
     <>
-    <div className="coin">
-      <h1>{coin.name} ({coin.symbol.toUpperCase()})</h1>
-      <p>Current Price: ${coin.current_price.toFixed(2)}</p>
-      <p>Market Cap: ${coin.market_cap.toLocaleString()}</p>
-      <p>...other details...</p>
-    </div>
+      <div className="coin">
+        <div className="coin-detail">
+          <img src={coin.image} alt="" />
+          <h1>{coin.name} ({coin.symbol.toUpperCase()})</h1>
+          <div className="coin-specific">
+            <p>Market Cap Rank: {coin.market_cap_rank}</p>
+          <p>Current Price: $ {coin.current_price.toFixed(2)}</p>
+          <p>Market Cap: $ {coin.market_cap.toLocaleString()}</p>
+          <p>Market Cap percentage 24h: <span style={{ color: coin.market_cap_change_percentage_24h < 0 ? 'red' : 'green' }}>{coin.market_cap_change_percentage_24h.toFixed(2)}</span></p>
+          <p>High value 24h: $ {coin.high_24h}</p>
+          <p>Low value 24h $ {coin.low_24h}</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
